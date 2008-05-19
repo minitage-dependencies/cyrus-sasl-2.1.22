825d1823660e623be6a2c5fe6610706ad1e3b612
kiorky <devnull@localhost>
2008-05-19 10:55:12 +0200

    0.4 changes

--- hooks/setenv.py
+++ hooks/setenv.py
@@ -1,12 +1,7 @@
 import os
-
-os_ldflags=''
-uname=os.uname()[0]
-if uname == 'Darwin':
-    os_ldflags=' -mmacosx-version-min=10.5.0'
-
 def getsaslenv(options=None,buildout=None):
     # patch Makefile
+    os.chdir(options['compile-directory'])
     for dir in ['.','include','lib','saslauthd','man','sasldb','plugins','pwcheck','utils' ]:
         oldpwd=os.getcwd()
         os.chdir(dir)
@@ -16,10 +11,23 @@ def getsaslenv(options=None,buildout=None):
         lines=file.readlines()
         for i,line in enumerate(lines):
             if line[:8] == 'framedir':
-                lines[i]='framedir = %s/include\n' %options['location']
+                lines[i]='framedir = %s/include\n' % options['location']
         file=open('/'.join((os.getcwd(),'Makefile')),'w+')
         file.writelines(lines)
         file.close()
         os.chdir(oldpwd)
+
+def getsaslenvb(options=None,buildout=None):
+    os.system(
+        'sed -e "s/\$ac_cache_corrupted/ [[ "a" == "b" ]] /g" -i %s' % (
+            os.path.join(options['compile-directory'],'saslauthd','configure')
+        )
+    )
+    os.system(
+        'sed -e "s/\$ac_cache_corrupted/ [[ "a" == "b" ]] /g" -i %s' % (
+            os.path.join(options['compile-directory'],'configure')
+        )
+    )
+    import pdb;pdb.set_trace()  ## Breakpoint ##
 
 # vim:set ts=4 sts=4 et  :
